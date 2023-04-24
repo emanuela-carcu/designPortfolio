4c7fed4c78985356f47a581955d82bb6cb7bff13
carcu <emanuela.carcu01@e-uvt.ro>
2023-04-24 16:26:06 +0300

    Initial commit

---
+++ src/main/java/com/portfolio/designPortfolio/util/DesignPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.designPortfolio.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/designPortfolio/DesignPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.designPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
